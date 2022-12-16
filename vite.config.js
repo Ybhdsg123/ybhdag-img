4e7ec5edd5aebce08bc34b32d6a7295e74adf8f1
zhs <1654134943@qq.com>
2022-12-16 14:05:07 +0800

    自动化部署4

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  publicPath: './',
   resolve:{
     alias: {
       '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
