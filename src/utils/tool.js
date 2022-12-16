389ff610c5c9b5d94a19117e208613e67e6c36c6
zhs <1654134943@qq.com>
2022-12-16 11:46:43 +0800

    打包测试线上图片显示

--- src/utils/tool.js
+++ src/utils/tool.js
@@ -1,23 +1,35 @@
-import { onMounted,watchEffect,ref } from "vue";
-
-export const getClinentWidth = ()=>{
-// 默认宽度
-let clientWidth = ref(790)
-// 屏幕变化时动态获取屏幕宽度
-onMounted(() => {
-  clientWidth.value = document.documentElement.clientWidth
-  // window.onresize = () => {
-  //   clientWidth.value = document.documentElement.clientWidth;
-  // };
-});
-// 监听屏幕宽度
-watchEffect(()=>{
-  window.onresize = () => {
-    clientWidth.value = document.documentElement.clientWidth;
-  };
-  // clientWidth.value = document.documentElement.clientWidth
-})
-
-
-return clientWidth.value
+/**
+ * @description: 瀑布流排法 等宽不等高  
+ * @param {*} data 图片数组 [{img:imgurl}]
+ * @param {*} clientW 图片的宽度
+ * @param {*} columnNums columnNums 列数
+ * @param {*} gap gap 间隔 
+ * @return {*}
+ */
+export function useWaterfall(data,clientW, columnNums,gap) {
+  let pW = clientW - gap * (columnNums - 1); // 总宽度
+  const columnHeights = []; // 列的高度
+  let newList = JSON.parse(JSON.stringify(data)); // 深拷贝数据
+  newList.length > 0 &&
+    newList.forEach((obj, i) => {
+      let index = i % columnNums; // 遍历的图片和列数取余
+      obj.w = pW / columnNums; // 图片宽度
+      // 创建图片对象获取图片的宽高
+      let img = new Image();
+      img.src = obj.img;
+      obj.h = Number((img.height * (obj.w / img.width)).toFixed(0));
+      obj.left = index * (obj.w + gap); //左边距离 2列的话，1/3/5距离左边距离为0，2/4/6距离左边距离为图片的宽度+间隔距离
+      obj.top = columnHeights[index] + gap || 0; // 定位高度
+      // 第一张和第二张距离顶部的高度为0
+      if (isNaN(columnHeights[index])) {
+        columnHeights[index] = obj.h;
+      } else {
+        // 第二张往后 找出前面的高度最小的索引
+        index = columnHeights.indexOf(Math.min(...columnHeights));
+        obj.left = index * (pW / columnNums + gap); // 改变每一张距离左边的距离
+        obj.top = columnHeights[index] + gap || 0; // 距离上面的距离
+        columnHeights[index] = obj.h + columnHeights[index] + gap; // 累加高度
+      }
+    });
+  return newList;
 }
--- src/views/index.vue
+++ src/views/index.vue
@@ -17,6 +17,7 @@
           :src="obj.img"
           alt=""
           mode="widthFix"
+          class="my-img"
         />
       </div>
     </div>
@@ -25,11 +26,12 @@
 
 <script setup>
 import { ref, watchEffect, onMounted } from "vue";
+import { useWaterfall } from "@/utils/tool";
 let clientW = ref(); // 屏幕宽度
 let clientH = ref(); // 屏幕高度
 let arrImgs = ref([]); // 获取的图片数据
 let columnNums = ref(2); // 要几列展示
-const gap = 8; // 图片之间的间隔
+const gap = 13; // 图片之间的间隔
 // 监听屏幕宽度
 watchEffect(() => {
   clientW.value = document.documentElement.clientWidth;
@@ -44,54 +46,34 @@ onMounted(() => {
 // 获取所有图片
 const getImgs = () => {
   let arrimg = [];
-  const objImgs = import.meta.glob("../../public/hunli/**", {
+  const objImgs = import.meta.glob("@/assets/hunli/**", {
     eager: true,
   });
   for (let key in objImgs) {
-    arrimg.push({ img: objImgs[key].default });
+    arrimg.push({ img: "../../" + objImgs[key].default });
   }
-  arrImgs.value = waterfall(arrimg, columnNums.value);
+  console.log(arrimg);
+  // 计算图片的大小和定位 瀑布流
+  arrImgs.value = useWaterfall(arrimg, clientW.value, columnNums.value, gap);
+  console.log(arrImgs.value);
 };
 getImgs();
-// 瀑布流 等宽不等高
-function waterfall(data, columnNums) {
-  let pW = clientW.value - gap * (columnNums - 1); // 总宽度
-  const columnHeights = []; // 列的高度
-  let newList = JSON.parse(JSON.stringify(data)); // 深拷贝数据
-  newList.length > 0 &&
-    newList.forEach((obj, i) => {
-      let index = i % columnNums; // 遍历的图片和列数取余
-      obj.w = pW / columnNums; // 图片宽度
-      // 创建图片对象获取图片的宽高
-      let img = new Image();
-      img.src = obj.img;
-      obj.h = Number((img.height * (obj.w / img.width)).toFixed(0));
-      obj.left = index * (obj.w + gap); //左边距离 2列的话，1/3/5距离左边距离为0，2/4/6距离左边距离为图片的宽度+间隔距离
-      // obj.top = columnHeights[index] + gap || 0; // 定位高度
-      // 第一张和第二张距离顶部的高度为0
-      if (isNaN(columnHeights[index])) {
-        columnHeights[index] = obj.h;
-      } else {
-        // 第二张往后 找出前面的高度最小的索引
-        index = columnHeights.indexOf(Math.min(...columnHeights));
-        console.log(index);
-        obj.left = index * (pW / columnNums + gap); // 改变每一张距离左边的距离
-        obj.top = columnHeights[index] + gap || 0; // 距离上面的距离
-        columnHeights[index] = obj.h + columnHeights[index] + gap; // 累加高度
-      }
-    });
-  return newList;
-}
 </script>
 
 <style scoped>
-.img-box {
-  width: 100vw;
-  height: 100vh;
-}
+/* .img-box {
+  width: 100%;
+  height: 100%;
+  padding: 13px;
+  box-sizing: border-box;
+} */
 #list {
+  position: relative;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
+}
+.my-img {
+  border-radius: 13px;
 }
 </style>
