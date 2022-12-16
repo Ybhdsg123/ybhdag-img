c6f582c622eeb3a8e239bb3b75f67417185f3b6a
zhs <1654134943@qq.com>
2022-12-16 15:11:01 +0800

    自动化部署10

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/ybhdag-img/", // 部署在  GitHub pages 上时，找不到js和css文件时，需要指定这个base路径，然后所有资源的路径都将据此配置重写
+  base: "/ybhdag-img/", // 部署在  GitHub pages 上时，找不到js和css文件时，需要指定这个base路径，然后所有资源的路径都将据此配置重写
   plugins: [vue()],
   resolve:{
     alias: {
