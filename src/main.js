3bb2bf9fed5033929bad48d85def83aec10b4094
Ybhdsg123 <Ybhdsg123@users.noreply.github.com>
2022-12-16 07:08:32 +0000

    deploy: 0de6c7aedeac2cd02d05a31649ac5327759bdee8

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import '@/style/style.css'
-import App from './App.vue'
-import router from '@/router'
-
-const app = createApp(App) // 创建vue实例
-
-app.use(router) // // 路由注册
-app.mount('#app')
